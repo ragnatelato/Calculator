825a7969907ed39432269d2ee829f7d1a8027603
Dany <daniele.culacciati@gmail.com>
2020-04-24 20:34:24 +0200

    some small implements

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,14 +11,29 @@ import com.example.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView operations = findViewById(R.id.operations);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setImmersive(true);
 
-//        mAdView = findViewById(R.id.adView);
+//        button = findViewById(R.id.button_comma);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d("MainActivity", "Ciao Dany!");
+//            }
+//        });
+    }
+
+    public void onClickSum(View view) {
+        operations.append("+");
+        Log.d("MainActivity", "The result is: " + (10 + 33));
+    }
+
+
+    //        mAdView = findViewById(R.id.adView);
 //        AdRequest adRequest = new AdRequest.Builder().build();
 //        mAdView.loadAd(adRequest);
 //
@@ -26,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
 //        });
 
 
-        //    AdSize adSize = new AdSize(300, 50);
+    //    AdSize adSize = new AdSize(300, 50);
 //    private AdView mAdView;
 
 //    mAdView.setAdListener(new AdListener() {
@@ -63,6 +81,5 @@ public class MainActivity extends AppCompatActivity {
 //        }
 //    });
 
-    }
 
 }
