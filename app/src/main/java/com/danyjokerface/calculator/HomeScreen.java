519bc3eae09b25103637a399606550f5366734fc
Dany <daniele.culacciati@gmail.com>
2020-04-25 20:03:12 +0200

    some small implements

--- app/src/main/java/com/danyjokerface/calculator/HomeScreen.java
+++ app/src/main/java/com/danyjokerface/calculator/HomeScreen.java
@@ -1,63 +1,38 @@
 package com.danyjokerface.calculator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeScreen extends AppCompatActivity {
-
-//    TextView operations = findViewById(R.id.operations);
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
         Log.d(HomeScreen.class.getName(), "sono nell'onCreate!");
+    }
 
-//        button = findViewById(R.id.button_comma);
-//        button.setOnClickListener(new View.OnClickListener() {
+//    public void onResume() {
+//        super.onResume();
+//        View author = findViewById(R.id.author);/
+//        author.setOnClickListener(new View.OnClickListener() {
 //            @Override
-//            public void onClick(View v) {
-//                Log.d("MainActivity", "Ciao Dany!");
+//            public void onClick(View view) {
+//                //TODO mettere condizione se si inserisce la mia data di nascita
+//                Intent intent = new Intent(getApplicationContext(), FirstQuizEasterEgg.class);
+//                startActivity(intent);
 //            }
 //        });
-    }
-
-    @Override
-    protected void onStart() {
-        Log.d(HomeScreen.class.getName(), "sono nell'onStart!");
-        super.onStart();
-    }
-
-    @Override
-    protected void onPause() {
-        Log.d(HomeScreen.class.getName(), "sono nell'onPause!");
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        Log.d(HomeScreen.class.getName(), "sono nell'onStop!");
-        super.onStop();
-    }
-
-    @Override
-    protected void onRestart() {
-        Log.d(HomeScreen.class.getName(), "sono nell'onRestart!");
-        super.onRestart();
-    }
-
-    @Override
-    protected void onDestroy() {
-        Log.d(HomeScreen.class.getName(), "sono nell'onDestroy!");
-        super.onDestroy();
-    }
-
-//    public void onClickSum(View view) {
-//        operations.append("+");
-//        Log.d("MainActivity", "The result is: " + (10 + 33));
+//
 //    }
+
+    public void onClickAuthor(View view) {
+        //TODO mettere condizione se si inserisce la mia data di nascita
+        Intent intent = new Intent(getApplicationContext(), FirstQuizEasterEgg.class);
+        startActivity(intent);
+    }
 
 }
